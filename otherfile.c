8f6484fdec1bbd7d095249643dfa4a1dfea9a658
Dandhy “YO SOY” EsparzaBar <283322450+m9228304320-rgb@users.noreply.github.com>
2026-09-10 02:31:01 -0600

    Mejoras en otherfile.c: documentación, mensajes de error y código defensivo

--- otherfile.c
+++ otherfile.c
@@ -1,21 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "otherfile.h"
 
+/**
+ * function_somewhere_else - Función de demostración que ilustra 
+ * funciones no testeadas en el proyecto
+ * @argument: Parámetro de entrada
+ *
+ * Nota: Esta función nunca debería ser llamada en pruebas unitarias.
+ * Si se ejecuta, indica un error en el flujo de control.
+ */
 void function_somewhere_else(int argument) {
-    /* does not get called, this illustrates a function elsewher in your
-     * project. We abort here to illustrate that this one really never gets
-     * called */
-    printf("..... %s wasn't supposed to be called\n", __func__);
+    fprintf(stderr, "ERROR: %s() was unexpectedly called with argument: %d\n", 
+            __func__, argument);
     abort();
 }
 
+/**
+ * database_id_exists - Verifica si un ID existe en la base de datos
+ * @id: ID a verificar
+ *
+ * Return: true si el ID existe, false en caso contrario
+ *
+ * Nota: Esta es una función ficticia que simula conectividad a BD.
+ * En pruebas unitarias debe ser mockeada.
+ */
 bool database_id_exists(unsigned int id) {
-    /* let's pretend this is the db connection code and whatnot */
-
-    /* we abort here to illustrate that it's really the mocked function that
-     * gets called, not this one */
-    printf("..... %s wasn't supposed to be called\n", __func__);
+    fprintf(stderr, "ERROR: %s() was unexpectedly called with id: %u\n", 
+            __func__, id);
     abort();
-    return true;
+    return false;
 }
