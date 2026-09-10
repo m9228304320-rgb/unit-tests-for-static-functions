e02e788db2f389da59f1038005766f7d552cb87b
Dandhy “YO SOY” EsparzaBar <283322450+m9228304320-rgb@users.noreply.github.com>
2026-09-10 02:32:08 -0600

    Mejoras en example.c: documentación kernel-doc, validación mejorada y mensajes de error claros

--- example.c
+++ example.c
@@ -7,16 +7,32 @@
 #include "example.h"
 #include "otherfile.h"
 
+/**
+ * is_acceptable_id - Valida si un ID es aceptable
+ * @id: ID a validar
+ *
+ * Return: true si el ID es válido (entre 1000 y 10000 y no existe en BD),
+ *         false en caso contrario
+ *
+ * Esta es una función estática de utilidad usada por some_function().
+ */
 static bool is_acceptable_id(unsigned int id) {
 
     if (database_id_exists(id))
-        return 0;
+        return false;
 
     return id > 1000 && id < 10000;
 }
 
+/**
+ * some_function - Procesa un argumento llamando a otras funciones
+ * @argument: Argumento a procesar
+ *
+ * Esta función ilustra cómo las funciones pueden depender de otras
+ * que pueden ser mockeadas en pruebas.
+ */
 void some_function(int argument) {
-    printf("..... %s wasn't supposed to be called\n", __func__);
+    fprintf(stderr, "Processing argument: %d\n", argument);
     function_somewhere_else(argument);
 }
 
@@ -25,10 +41,18 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Usage: %s <id>\n", basename(argv[0]));
         return 1;
     }
+    
     int id = atoi(argv[1]);
-    int is_acceptable = is_acceptable_id(id);
+    
+    /* Validar que el ID sea un número válido */
+    if (id < 0) {
+        fprintf(stderr, "Error: ID must be a positive number\n");
+        return 1;
+    }
+    
+    bool is_acceptable = is_acceptable_id((unsigned int)id);
 
-    printf("ID is acceptable: %d\n", is_acceptable);
+    printf("ID is acceptable: %s\n", is_acceptable ? "yes" : "no");
 
     return 0;
 }
