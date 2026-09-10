71636d32310cbeca0a3776b189579d18fede9af8
Dandhy “YO SOY” EsparzaBar <283322450+m9228304320-rgb@users.noreply.github.com>
2026-09-10 02:43:14 -0600

    Add header guards to otherfile.h

--- otherfile.h
+++ otherfile.h
@@ -1,4 +1,9 @@
+#ifndef OTHERFILE_H
+#define OTHERFILE_H
+
 #include <stdbool.h>
 
 void function_somewhere_else(int argument);
 bool database_id_exists(unsigned int id);
+
+#endif /* OTHERFILE_H */
